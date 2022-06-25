99fb25908056ec22d6a53f308e2a40da68ef50ff
阿杰 <53169820+laihaojie@users.noreply.github.com>
2022-06-25 10:05:33 +0800

    chore: update

--- fixture/a.ts
+++ fixture/a.ts
@@ -4,3 +4,15 @@ export const a: string = 1
 export interface Foo {
   a: string
 }
+
+function ApiProperty() { }
+
+class CreatePostDto {
+  // @ts-ignore
+  @ApiProperty()
+  title: string
+
+  // @ts-ignore
+  @ApiProperty()
+  content: string
+}
