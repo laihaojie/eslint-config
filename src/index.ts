570385dbc173026d8b9f5cc4a75e0002f9af05f4
阿杰 <53169820+laihaojie@users.noreply.github.com>
2023-11-25 14:58:14 +0800

    chore: update

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,33 @@
-import { jie } from './factory'
+import antfu, { Awaitable, FlatConfigItem, OptionsConfig, UserConfigItem } from "@antfu/eslint-config";
 
-export * from './configs'
-export * from './factory'
-export * from './globs'
-export * from './types'
-export * from './utils'
+export default function jie (options?: OptionsConfig & FlatConfigItem, ...userConfigs: Awaitable<UserConfigItem | UserConfigItem[]>[]) {
 
-export default jie
+
+  const customOptions : OptionsConfig & FlatConfigItem= {
+    rules: {}
+  }
+
+
+  return antfu(deepMerge(customOptions, options), ...userConfigs)
+}
+
+
+function deepMerge(target: any, source: any) {
+  if (isObject(target) && isObject(source)) {
+    for (const key in source) {
+      if (isObject(source[key])) {
+        if (!target[key]) Object.assign(target, { [key]: {} })
+        deepMerge(target[key], source[key])
+      } else {
+        Object.assign(target, { [key]: source[key] })
+      }
+    }
+  }
+  return target
+}
+
+function isObject(item: any) {
+  return item && typeof item === 'object' && !Array.isArray(item)
+}
+
+console.log(1)
--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: [
     'src/index.ts',
-    'src/cli.ts',
+    // 'src/cli.ts',
   ],
   shims: true,
 })
